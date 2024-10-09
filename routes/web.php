36831d843770578732510bb4c51ac78b9bcbc353
Lucas Lacerda <lucaslacerdaandrey@gmail.com>
2024-10-09 20:29:50 -0300

    adicionado namespace e group para as rotas dos controllers

--- routes/web.php
+++ routes/web.php
@@ -6,34 +6,41 @@ use App\Http\Controllers\BookController;
 use \App\Http\Controllers\BookRentalController;
 
 // --------- user ---------
-Route::get('/userIndex', [UserController::class, 'index']);
+Route::namespace('User')->group(function (){
+    Route::get('/userIndex', [UserController::class, 'index']);
 
-Route::post('/users', [UserController::class, 'store']);
+    Route::post('/users', [UserController::class, 'store']);
 
-Route::get('/users/{user}', [UserController::class, 'show']);
+    Route::get('/users/{user}', [UserController::class, 'show']);
 
-Route::patch('/users/{user}', [UserController::class, 'update']);
+    Route::patch('/users/{user}', [UserController::class, 'update']);
 
-Route::delete('/users/{user}', [UserController::class, 'delete']);
+    Route::delete('/users/{user}', [UserController::class, 'delete']);
+});
 
 // --------- book ---------
-Route::get('/bookIndex', [BookController::class, 'index']);
+Route::namespace('Book')->group(function (){
+    Route::get('/bookIndex', [BookController::class, 'index']);
 
-Route::post('/books', [BookController::class, 'store']);
+    Route::post('/books', [BookController::class, 'store']);
 
-Route::get('/books/{book}', [BookController::class, 'show']);
+    Route::get('/books/{book}', [BookController::class, 'show']);
 
-Route::patch('/books/{book}', [BookController::class, 'update']);
+    Route::patch('/books/{book}', [BookController::class, 'update']);
 
-Route::delete('/books/{book}', [BookController::class, 'delete']);
+    Route::delete('/books/{book}', [BookController::class, 'delete']);
+});
 
 // --------- book rental ---------
-Route::get('/bookRentalIndex', [BookRentalController::class, 'index']);
+Route::namespace('Book_rental')->group(function (){
+    Route::get('/bookRentalIndex', [BookRentalController::class, 'index']);
 
-Route::get('/bookRentals/{bookRental}', [BookRentalController::class, 'show']);
+    Route::get('/bookRentals/{bookRental}', [BookRentalController::class, 'show']);
 
-Route::post('/bookRentals', [BookRentalController::class, 'store']);
+    Route::post('/bookRentals', [BookRentalController::class, 'store']);
 
-Route::patch('/bookRentals/{bookRental}', [BookRentalController::class, 'update']);
+    Route::patch('/bookRentals/{bookRental}', [BookRentalController::class, 'update']);
 
-Route::delete('/bookRentals/{bookRental}', [BookRentalController::class, 'delete']);
+    Route::delete('/bookRentals/{bookRental}', [BookRentalController::class, 'delete']);
+
+});
